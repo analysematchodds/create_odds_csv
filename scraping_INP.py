bf1758bc180d28b6f0d9895f58ee03760a490740
analysematchodds <analysematchodds@outlook.com>
2025-01-29 23:04:36 +0300

    Update scraping_INP.py

--- scraping_INP.py
+++ scraping_INP.py
@@ -107,7 +107,7 @@ def get_iddaa_data(iddaa_hafta):
             found_next_league = False
             buffer = ''
             
-            current_league_string = "Premier Lig"
+            current_league_string = "İngiltere - Premier Lig"
             current_league_slug = "İNP"
             
             for chunk in response.iter_content(chunk_size=4096, decode_unicode=True):
